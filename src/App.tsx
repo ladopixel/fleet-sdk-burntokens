af59acdd730129225de243dcc5ea637c960c36d4
ladopixel <ladopixel@protonmail.com>
2024-03-06 11:22:19 +0100

    sendnft with Fleet SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,67 @@
-import * as React from "react"
+import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Text,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+import { OutputBuilder, TransactionBuilder } from "@fleet-sdk/core";
+
+declare global {
+  interface Window {
+    ergoConnector: any;
+  }
+}
+declare var ergo: any;
+var connected: any;
+
+export const App = () => {
+
+  const [tx, setTx] = useState('...');
+
+  useEffect(() => {
+    send_token();
+  }, []);
+
+  async function send_token(): Promise<void> { 
+    connected = await window.ergoConnector.nautilus.connect(); 
+    if (connected) {
+      const height = await ergo.get_current_height();
+      const unsignedTx = new TransactionBuilder(height)
+        .from(await ergo.get_utxos())
+        .to(
+          new OutputBuilder(
+            "1000000", "9gBYZrMRNX66uN5VhLnTw6absspsarXPxcWSi5fuE5EesBfQC6s"
+          )
+          .addTokens({ 
+            tokenId: 
+              "bb010d9816e8371b6d3889eb2afeecfd8d79c391aa2ed2ff908774a9f3e32c2d",
+            amount: "1", 
+          }) 
+        )
+        .sendChangeTo(await ergo.get_change_address())
+        .payMinFee()
+        .build()
+        .toEIP12Object();
+      const signedTx = await ergo.sign_tx(unsignedTx);
+      const txId = await ergo.submit_tx(signedTx);
+      setTx(txId);
+    }
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Text as='mark'>
+            tx ID: {tx}
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+};
